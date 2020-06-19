18d6d395ea9c62cb1467a5f31772c26651bf96f8
dehunter456 <dehunter456@gmail.com>
2020-06-19 09:01:11 -0500

    Updates

---
+++ precache-manifest.4f78066ad09f11a80e74fecd7b90df16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eddffcec08a096e11751d2fcbd68d23",
+    "url": "/react-virtualized-draggable-cols/index.html"
+  },
+  {
+    "revision": "8fe04677fd13cb950992",
+    "url": "/react-virtualized-draggable-cols/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "8fe04677fd13cb950992",
+    "url": "/react-virtualized-draggable-cols/static/js/2.61c7e573.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-virtualized-draggable-cols/static/js/2.61c7e573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e342fb5e1fd88e4dda2",
+    "url": "/react-virtualized-draggable-cols/static/js/main.751ddd16.chunk.js"
+  },
+  {
+    "revision": "a1231dd713a3e6bfd4ab",
+    "url": "/react-virtualized-draggable-cols/static/js/runtime-main.5c7d0817.js"
+  }
+]);
